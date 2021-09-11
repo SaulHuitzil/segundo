6aeff2e72c1ab09bf58437fb2b34af5d9035df5f
saulhuitzil@gmail.com <saulhuitzil@gmail.com>
2021-09-11 01:13:17 -0500

    escribiendo la segunda version

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@ namespace mensajes
             Console.WriteLine("Hello World!");
             Console.WriteLine("Este es el primer mensaje");
             Console.WriteLine("Este es el segundo mensaje");
+            T01();
+        }
+
+        static void T01() {
+          for (int i = 0; i < 10; i++) {
+            Console.WriteLine($"El valor de i es {i}");
+          }
         }
     }
 }
