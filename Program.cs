e780e9f120b613e00c5ba4dcc8d51bba3571f226
saulhuitzil@gmail.com <saulhuitzil@gmail.com>
2021-09-11 00:57:39 -0500

    Escribiendo la primera version

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace mensajes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Este es el primer mensaje");
+            Console.WriteLine("Este es el segundo mensaje");
+        }
+    }
+}
